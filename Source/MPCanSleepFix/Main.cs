b832232b97f189b1d42ae1cc18aa623ac06b4da9
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-06-14 14:06:11 +1200

    v1.6

--- Source/MPCanSleepFix/Main.cs
+++ Source/MPCanSleepFix/Main.cs
@@ -2,40 +2,39 @@
 using RimWorld;
 using Verse;
 
-namespace MPCanSleepFix
+namespace MPCanSleepFix;
+
+[StaticConstructorOnStartup]
+public static class MPCanSleepFix
 {
-    [StaticConstructorOnStartup]
-    public static class MPCanSleepFix
+    private const int BufferTicks = 1_000;
+
+    static MPCanSleepFix()
     {
-        private const int BufferTicks = 1_000;
+        Harmony harmony = new("NachoToast.MPCanSleepFix");
 
-        static MPCanSleepFix()
+        harmony.Patch(
+            original: AccessTools.Method(
+                type: typeof(JobGiver_GetRest),
+                name: nameof(JobGiver_GetRest.GetPriority)),
+            prefix: new HarmonyMethod(
+                methodType: typeof(MPCanSleepFix),
+                methodName: nameof(GetRestPriority_Prefix)));
+    }
+
+    private static void GetRestPriority_Prefix(Pawn pawn)
+    {
+        int? sleepTick = pawn?.mindState?.canSleepTick;
+
+        if (!sleepTick.HasValue)
         {
-            Harmony harmony = new Harmony("NachoToast.MPCanSleepFix");
-
-            harmony.Patch(
-                original: AccessTools.Method(
-                    type: typeof(JobGiver_GetRest),
-                    name: nameof(JobGiver_GetRest.GetPriority)),
-                prefix: new HarmonyMethod(
-                    methodType: typeof(MPCanSleepFix),
-                    methodName: nameof(GetRestPriority_Prefix)));
+            return;
         }
 
-        private static void GetRestPriority_Prefix(Pawn pawn)
+        if (Find.TickManager.TicksGame + BufferTicks < sleepTick)
         {
-            int? sleepTick = pawn?.mindState?.canSleepTick;
-
-            if (!sleepTick.HasValue)
-            {
-                return;
-            }
-
-            if (Find.TickManager.TicksGame + BufferTicks < sleepTick)
-            {
-                Log.Message($"[MPCanSleepFix] Fixing pawn {pawn} (game = {Find.TickManager.TicksGame}, sleep = {sleepTick})");
-                pawn.mindState.canSleepTick = Find.TickManager.TicksGame;
-            }
+            Log.Message($"[MPCanSleepFix] Fixing pawn {pawn} (game = {Find.TickManager.TicksGame}, sleep = {sleepTick})");
+            pawn.mindState.canSleepTick = Find.TickManager.TicksGame;
         }
     }
 }
